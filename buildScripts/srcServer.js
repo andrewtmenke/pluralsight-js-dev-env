95e94cb34a6b7040934e8adf743fe66360fb2963
Andrew Menke <andrewmenke3@gmail.com>
2020-01-30 13:05:12 -0700

    Finished Module 11 - HTTP Calls

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,6 +4,8 @@ import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev.js';
 
+/* eslint-disable no-console */
+
 const port = 3000;
 const app = express();
 const compiler = webpack(config);
@@ -15,6 +17,15 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.get('/', function(req, res) {
    res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.get('/users', function(req, res) {
+   // Hard coding for simplicity. Pretend this hits a real database.
+   res.json([
+      { "id":0,"firstName":"Andrew","lastName":"Menke","email":"a@gmail.com" },
+      { "id":1,"firstName":"Joe","lastName":"Featherstone","email":"b@gmail.com" },
+      { "id":2,"firstName":"Heidi","lastName":"Featherston","email":"c@gmail.com" }
+   ]);
 });
 
 app.listen(port, function(err){
